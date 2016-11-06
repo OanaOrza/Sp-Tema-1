8b9239cd237974f860903b95be293aa9778828b4
OanaOrza <oana.orza@yahoo.com>
2016-11-06 12:07:17 +0200

    Add files via upload

---
+++ gr_423D.m
@@ -0,0 +1,14 @@
+function gr_423D(n) 
+B = randn(n) %initializez matricea gaussiana
+k=1; 
+for i=1:n %parcurgerea matricei pe linii 
+    for j=1:n %parcurgerea matricei pe coloane
+        
+        if B(i,j)<0 %daca elementul de pe linia i si coloana j este negativ, atunci il retinem intr-un vector
+            v(k)=B(i,j);
+            k=k+1; 
+    end
+    end
+    
+end
+
